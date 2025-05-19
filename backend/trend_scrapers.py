d3026cdb20c72f8e09d12dbbd29e605e31b1eda8
Vulcanric <jneric49@gmail.com>
2025-05-19 05:09:29 +0100

    Add scraping functionalities for Youtube and Reddit

---
+++ backend/trend_scrapers.py
@@ -0,0 +1,56 @@
+""" Scrape popular platforms for trending content
+"""
+import json
+from bs4 import BeautifulSoup
+from agents import trend_extractor
+from typing import List
+
+
+def get_reddit_trending(raw_data) -> List[str]:
+    """ Scrape reddit for trending/popular contents
+        Returns list of topics
+    """
+    # url = "https://www.reddit.com/r/all/hot.json?limit=5"
+    if not raw_data:
+        return []
+
+    data = json.loads(raw_data)
+    titles = [post["data"]["title"] for post in data["data"]["children"]]
+    print("REDDIT: ", titles)
+    return titles
+    # json_trends = trend_extractor(raw_data)
+    # trends = json.loads(json_trends)
+    # return trends
+
+
+def get_youtube_trending(raw_data: str) -> List[str]:
+    """ Scrape youtube for trending contents
+    """
+    if not raw_data:
+        return []
+
+    # url = "https://www.youtube.com/feed/trending"
+    soup = BeautifulSoup(raw_data, "html.parser")  # Parse HTML first
+
+    titles = []
+    for script in soup.find_all("script"):
+        if "var ytInitialData" in script.text:
+            start = script.text.find("var ytInitialData") + len("var ytInitialData = ")
+            end = script.text.rfind("};") + 1
+            raw_json = script.text[start:end]
+            data = json.loads(raw_json)
+            videos = data["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]["tabRenderer"]\
+                ["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]
+            for item in videos:
+                try:
+                    title = item["videoRenderer"]["title"]["runs"][0]["text"]
+                    print("TITLE: ", title)
+                    titles.append(title)
+                    if len(titles) >= 30:  # Limit
+                        break
+                except:
+                    continue
+            break
+
+    print("YOUTTUBE: ", titles)
+    return titles
