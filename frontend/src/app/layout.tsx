0c79f298a89025fd580b3ed4865b38acddd1d936
Vulcanric <jneric49@gmail.com>
2025-05-24 18:31:07 +0100

    Removed uncomfortable ember/fire animation from page background. Used clean soothing gradient rather

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,22 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' text-lg bg-transparent'}>
-        {/* Animated dispersed fire/ember background */}
-        <div className="pointer-events-none fixed inset-0 -z-10">
-          {/* Large center ember */}
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[120vw] h-[90vh] bg-gradient-to-t from-orange-200/3 via-yellow-100/2 to-transparent rounded-full blur-[200px] animate-pulse"></div>
-          {/* Top ember */}
-          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[80vw] h-[40vh] bg-gradient-to-b from-orange-100/2 via-yellow-50/2 to-transparent rounded-full blur-[140px] animate-pulse"></div>
-          {/* Bottom ember */}
-          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[100vw] h-[50vh] bg-gradient-to-t from-orange-200/3 via-yellow-100/2 to-transparent rounded-full blur-[180px] animate-pulse"></div>
-          {/* Left vertical ember */}
-          <div className="absolute top-1/4 left-0 w-[30vw] h-[70vh] bg-gradient-to-tr from-orange-100/2 via-yellow-50/2 to-transparent rounded-full blur-[140px] animate-pulse"></div>
-          {/* Right vertical ember */}
-          <div className="absolute top-1/4 right-0 w-[30vw] h-[70vh] bg-gradient-to-tl from-orange-100/2 via-yellow-50/2 to-transparent rounded-full blur-[140px] animate-pulse"></div>
-        </div>
+      <body className={inter.className + ' h-screen text-md'}>
         {children}
-        <Toaster position="top-right" />
+        <Toaster position="top-right" containerClassName="text-base" />
       </body>
     </html>
   );
