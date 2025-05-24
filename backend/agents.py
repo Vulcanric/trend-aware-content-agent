97234e3c9382aa5550b247a30a69c89eb3b91e3d
Vulcanric <jneric49@gmail.com>
2025-05-24 18:17:31 +0100

    Refactor content_generator and trend_extractor for improved clarity and structure

--- backend/agents.py
+++ backend/agents.py
@@ -2,7 +2,8 @@
 import os
 from together import Together
 from dotenv import load_dotenv
-from models import Trend
+from bright_data import search_with_bright_data
+# from time import sleep
 
 
 load_dotenv()
@@ -17,21 +18,40 @@ def content_generator(trend: str, content_format: str, target_audience: str, moo
     """
     print("Content Generator: Running")
 
+    # sleep(20)
+    # return "I will not generate anything for you! 😤"
+
     if not trend:
         return "No trend provided."
 
-    details = f"Format: {content_format}. Audience: {target_audience}. Mood: {mood}"
+    SYSTEM_PROMPT = f"""
+    You are a world-class content creator skilled at creating the best and creative contents.
+    Your task is to generate unique and engaging content based on the provided trend.
+    You should consider the format, target audience, and tone specified in the request.
+    You should also ensure that the content is well-structured and formatted in a markdown syntax.
+    You should not include any irrelevant information or noise in your response.
+    RULE: Only return the content
+    """
+
+    details = f"Format: {content_format}. Audience: {target_audience}. Tone: {mood}"
+    info = search_with_bright_data(trend)  # Get more context on the trend
+    print("INFO: ", info)
+
     prompt = f"""
     Generate a unique content ({details}) for this trending topic: {trend}.
-    IMPORTANT RULE: Your response must be well structured and formatted in a markdown syntax
+
+    For more context, you can use the information below:
+    {
+        info if info else "No additional information available on this trend."
+    }
     """
 
     response = client.chat.completions.create(
-        model="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
+        model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
         messages=[
             {
                 "role": "system",
-                "content": "You're a world-class content creator"
+                "content": SYSTEM_PROMPT
             },
             {
                 "role": "user",
@@ -50,23 +70,24 @@ def trend_extractor(raw_data: str) -> str:
         Extracts trending topics from raw, impure, and unstructured data.
     """
     print("Trend Extractor: Running")
+
     prompt = f"""
     The data below contains trends.
     Your goal is to extract trending topics from it and refine it as needed.
     The data may contain noise, irrelevant information, or unstructured text.
     Your task is to identify and extract the relevant trending topics from the data.
     You should focus on the most relevant and popular topics that are currently trending.
-    You should also ensure that the extracted topics are in a clean and structured format.
-    You should not include any irrelevant information or noise in your response.
+    You should also ensure that the extracted topics are clean and must tell a story.
     You should return the extracted topics in a list format.
     You should only return the list of extracted topics.
-    Returned topics must not be less that 10
-    IMPORTANT RULE: Always reply in valid json format List[str], no code blocks.
+    Returned topics must not be less than 15.
+    Your response is going to be parsed by a JSON decoder, so don't tell me any other thing
+    IMPORTANT RULE: Always reply in valid json format List[str], no code blocks, no thought, and no explanation.
     ---
     {raw_data}
     """
     response = client.chat.completions.create(
-        model="meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
+        model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
         messages=[
             {
                 "role": "user",
